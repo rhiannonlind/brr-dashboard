f947f4c9917fe9d10c7c7f88c3d9d903bc24b258
rhiannonlind <133162388+rhiannonlind@users.noreply.github.com>
2025-05-08 16:01:57 +0100

    feat: Restructure app layout and implement Staff Directory
    - Move Staff Directory to dedicated page component
    - Move  staff data structure to it's own component for modularity
    - Add view state management for page navigation
    - Update Header component to handle view changes
    - Set up basic routing structure for main dashboard
    
    Sets active view to the dashboard and restructures the Staff Directory to allow more organised updates to the individual components

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import puppyImage from './assets/puppy.jpeg'
 
-
-export default function Header() {
+export default function Header({ onViewChange }) {
   return (
     <div className='fixed top-0 left-0 flex flex-col w-[20%] h-screen bg-gray-400'>
-    <div className='w-[100%]'>
-      <img src={puppyImage} alt='puppy' />
+      <div className='w-[100%]'>
+        <img src={puppyImage} alt='puppy' />
+      </div>
+      <div className='w-[100%] pt-3.5 flex flex-col gap-2'>
+        <button 
+          onClick={() => onViewChange('main')}
+          className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'
+        >
+          Dashboard
+        </button>
+        <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>Tickets</button>
+        <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>To-Do's</button>
+        <button 
+          onClick={() => onViewChange('staff-directory')}
+          className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer header-button staff-directory-button'
+        >
+          Staff Directory
+        </button>
+        <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>IT Request</button>
+      </div>
     </div>
-    <div className='w-[100%] pt-3.5 flex flex-col gap-2'>
-      <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>Dashboard</button>
-      <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>Tickets</button>
-      <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>To-Do's</button>
-      <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>Staff Directory</button>
-      <button className='bg-blue-500 text-white p-2 rounded-md w-[100%] hover:cursor-pointer'>IT Request</button>
-    </div>
-  </div>
   )
 }
---
+++ src/StaffDirectory.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import StaffDirectoryData from './StaffDirectoryData'
+import Header from './Header'
+import Banner from './Banner'
+
+export default function StaffDirectory() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <StaffDirectoryData />
+    </div>
+  )
+}
